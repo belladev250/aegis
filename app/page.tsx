09a6590d807eea3bd70be29328bd08e6f5b3876d
belladev250 <lissadev17@gmail.com>
2025-04-02 12:03:32 +0200

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { IoChevronForward } from "react-icons/io5";
+
 
 export default function Home() {
   return (
@@ -29,6 +31,74 @@ export default function Home() {
     <button className="bg-white text-maroon p-3 rounded-md">Join Our Community</button>
   </div>
   
+</div>
+
+{/* The second section area */}
+<div className="p-20 flex w-full space-x-16">
+
+<div className="space-y-6 w-1/2">
+  <div className="space-y-4">
+    <Image src='/community.png' alt="community" height={500} width={500} className="flex-shrink-0 object-cover"/>
+
+    <div className="space-y-6">
+      <p className="text-bold text-3xl text-navy">Aegis Research</p>
+      <p>Since 2014, the Aegis Trust with the financial support from the UK Department for International Development (DfID) and Swedish International Development Cooperation Agency (Sida) has provided competitive research grants to Rwandan researchers working on various topics examining Rwanda's past, 
+        present and future from various disciplinary perspectives that concern peacebuilding. </p>
+
+       <div className="flex space-x-2">
+      <p className="text-maroon">Learn more </p>
+      <IoChevronForward className="text-maroon text-lg"/>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+
+<div className="w-1/2 space-y-4">
+
+<div className=" flex space-x-10  border -2 border-borderColor p-4 rounded-lg cursor-pointer  ">
+  <Image src='/about.png' alt="default" width={80} height={80}/>
+
+  <div className="flex mt-4 space-x-2">
+      <p className="text-maroon text-xl"> Aegis Working Papers</p>
+      <IoChevronForward className="text-maroon text-3xl "/>
+ </div> 
+
+</div>
+
+<div className=" flex space-x-10  border -2 border-borderColor p-4 rounded-lg cursor-pointer  ">
+  <Image src='/about.png' alt="default" width={80} height={80}/>
+
+  <div className="flex mt-4 space-x-2">
+      <p className="text-maroon text-xl"> Aegis Policy Briefs</p>
+      <IoChevronForward className="text-maroon text-3xl "/>
+ </div> 
+
+</div>
+
+<div className=" flex space-x-4  border -2 border-borderColor p-4 rounded-lg cursor-pointer  ">
+  <Image src='/about.png' alt="default" width={80} height={80}/>
+
+  <div className="flex mt-4 space-x-2">
+      <p className="text-maroon text-xl"> RPHE Journal articles and book chapters</p>
+      <IoChevronForward className="text-maroon text-3xl "/>
+ </div> 
+
+</div>
+
+<div className=" flex space-x-4  border -2 border-borderColor p-4 rounded-lg cursor-pointer  ">
+  <Image src='/about.png' alt="default" width={80} height={80}/>
+
+  <div className="flex mt-4 space-x-2">
+      <p className="text-maroon text-xl"> RPHE Journal articles and book chapters</p>
+      <IoChevronForward className="text-maroon text-3xl "/>
+ </div> 
+
+</div>
+
+</div>
+
 </div>
 
 
