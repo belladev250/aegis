0d00e44bd213beeb576d84c577f6cf87cd59c8d3
belladev250 <lissadev17@gmail.com>
2025-04-09 09:03:27 +0200

    pull

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,9 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const About = () => {
-    return (
-        <div className='relative z-40 min-h-screen bg-[#F7F2F2]'>
-            <div className="">
+    return ( 
+        <div className='absolute inset-0 bg-[rgb(247,242,242)]'>
             {/* Scrollable content area */}
             <div className='overflow-y-auto h-full pt-24 lg:pt-32'>
                 <div className="container mx-auto flex flex-col lg:flex-row px-4 sm:px-6 lg:px-8 py-8 font-sans text-gray-800 leading-relaxed">
@@ -116,7 +115,7 @@ const About = () => {
                 </div>
             </div>
         </div>
-        </div>
+        
     );
 }
 
